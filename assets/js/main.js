2fd9c3ee4ca7d5b813fe776b42a1b9ab0901abe4
Ada <1564505714@qq.com>
2026-06-09 22:37:02 +0800

    609

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 const headerNav = document.querySelector(".header-nav");
+const navBox = document.querySelectorAll(".nav-box");
 let lastScroll = 0;
 const debounce = 10;
+const Width = window.innerWidth;
+let currentHeight = (Width * 9) / 16;
 
 window.addEventListener("scroll", function () {
     let current = window.pageYOffset;
@@ -13,4 +16,16 @@ window.addEventListener("scroll", function () {
     }
 
     lastScroll = current;
+    console.log(current);
+    if (current > currentHeight / 3) {
+        headerNav.classList.add("add-color");
+        navBox.forEach((element) => {
+            element.classList.remove("add-shadow");
+        });
+    } else {
+        navBox.forEach((element) => {
+            element.classList.add("add-shadow");
+        });
+        headerNav.classList.remove("add-color");
+    }
 });
